acbbed55fbf105661a3df100f8b6323de9914ce3
ogbuehi <160658459+ogbuehi@users.noreply.github.com>
2024-08-23 11:54:45 +0100

    Update Buyer.java

--- src/main/java/com/javatask/BuyerService/model/Buyer.java
+++ src/main/java/com/javatask/BuyerService/model/Buyer.java
@@ -4,14 +4,18 @@ import jakarta.persistence.*;
 import lombok.Data;
 
 @Data
+@Table(name = "buyer")
 @Entity
 public class Buyer {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "buyer_id")
     private Integer id;
     private String username;
     private String password;
     private String email;
-    @ManyToMany
-    private Supplier supplier;
+    @ManyToMany(fetch = FetchType.LAZY, cascade = {CascadeType.PERSIST, CascadeType.MERGE, CascadeType.DETACH, CascadeType.REFRESH})
+    @JoinTable(name = "SUPPLIER MAPPING", joinColumns = @JoinColumn(name = "buyer_id"), 
+        inverseJoinColumns = @JoinColumn(name = "supplier_id"))
+    private List<Supplier> supplier;
 }
