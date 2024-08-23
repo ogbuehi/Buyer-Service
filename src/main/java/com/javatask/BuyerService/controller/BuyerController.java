cdde647efd5984685a7926694044ee6d2211ab7a
ogbuehi <160658459+ogbuehi@users.noreply.github.com>
2024-08-23 10:12:53 +0100

    Update BuyerController.java

--- src/main/java/com/javatask/BuyerService/controller/BuyerController.java
+++ src/main/java/com/javatask/BuyerService/controller/BuyerController.java
@@ -17,12 +17,16 @@ public class BuyerController {
     private final BuyerService buyerService;
 
     // To add a buyer to the database
+    // localhost:8080/api/buyer/add_buyer
+    // then add body in the request containing username, password and email to add buyer.
     @Secured("ROLE_USER")
     @PostMapping("/add_buyer")
     public ResponseEntity<String> addBuyer(@RequestBody BuyerSignUpDto buyerSignUpDto){
         return buyerService.addBuyer(buyerSignUpDto);
     }
     // To get the buyer's own criteria of manufacturers from the database
+    // localhost:8080/api/buyer/get_suppliers?pageNum=1&pageSize=5
+    // then add a body in the request containing the location, nature of business and manufacturing process to get result.
     @GetMapping("/get_suppliers")
     public ResponseEntity<SupplierResponse> getSpecificSuppliers
     (@RequestParam(value = "pageNum", defaultValue="0") int pageNum,
